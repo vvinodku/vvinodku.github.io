bc3a4fb42885601a936e00d516131a8744503f79
Vaishakh Vinod <63565828+vvinodku@users.noreply.github.com>
2020-08-10 13:27:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+let runningTotal = 0;
+let buffer = '0';
+let previousOperator;
+const screen  = document.querySelector(".answer")
+
+
+document.querySelector('.calc-button').addEventListener("click", function(event) {
+    buttonClick(event.target.innerText);   
+});
+
+function buttonClick(value) {
+    if(isNaN(parseInt(value))) {
+        handleSymbol(value);
+    } 
+    else {
+        handleNumber(value);
+    }
+    rerender();
+}
+
+function handleNumber(value) { 
+    if(buffer === '0') {
+        buffer = value;
+    } 
+    else {
+        buffer += value;
+    }
+}
+
+function handleSymbol(value) {
+    switch(value) {
+        case 'C' :
+            buffer = '0';
+            runningTotal = 0;
+            previousOperator = 0;
+            break;
+        case "=" :
+            if (previousOperator === null) {
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = "" + runningTotal;
+            runningTotal = 0;
+            break;
+        case "Back" :
+            if (buffer.length === 1) {
+                buffer = '0';
+            }
+            else {
+                buffer = buffer.substring(0, buffer.length-1);
+            }
+            break;
+        default:
+            handleMath(value);
+    }
+}
+
+function handleMath(value) {
+    if ( buffer === '0' ){
+        return ;
+    }
+
+    const intBuffer = parseInt(buffer);
+    if ( runningTotal ===0 ) {
+        runningTotal = intBuffer;
+    }
+    else {
+        flushOperation(buffer);
+    }
+
+    previousOperator = value
+    buffer = '0'
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === "+") {
+        runningTotal += intBuffer;
+    }
+
+    else if (previousOperator === "-") {
+        runningTotal -= intBuffer;
+    }
+
+    else if (previousOperator === "*") {
+        runningTotal *= intBuffer;
+    }
+
+    else {
+        runningTotal /= intBuffer;
+    }
+}
+
+function rerender() {
+    screen.innerText = buffer;
+}
